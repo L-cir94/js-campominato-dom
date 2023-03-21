0ca1cd6b2365e9bcd8baaf08e2f108f13bf49b31
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-21 14:34:24 +0100

    generate bombs 16

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,33 +1,34 @@
 /* 
 #Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
-ed emetto un messaggio in console con il numero della cella cliccata.
-##Consigli del giorno: 
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Eventuali validazioni e controlli possiamo farli anche in un secondo momento.
-##Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
-tra tre diversi livelli di difficoltà:
-con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, 
-divise in 10 caselle per 10 righe;
-con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, 
-divise in 9 caselle per 9 righe;
-con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, 
-divise in 7 caselle per 7 righe;
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco
+Attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo 
+l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare 
+problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della 
+difficoltà prescelta: le bombe.
+nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe 
+non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella:
+se il numero è presente nella lista dei numeri generati
+abbiamo calpestato una bomba
+la cella si colora di rosso e la partita termina.
+Altrimenti
+la cella cliccata si colora di azzurro
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero 
+massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono 
+    bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che 
+l’utente ha cliccato su una cella che non era una bomba.
 
 */
 
 /* strumenti
 
 */
-const numberBombs = 16
-cellNumb = 100
+
+
+
 const containerEl = document.querySelector('.container')
 const play = document.querySelector('.play');
 const reset = document.querySelector('.reset');
@@ -35,35 +36,48 @@ const reset = document.querySelector('.reset');
 //L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 //Ogni cella ha un numero progressivo, da 1 a 100.
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-play.addEventListener('click', function (e) {
+play.addEventListener('click', function () {
+
+    // recupero del valore selezionato
+let selectDaVerificare = document.getElementById("selectDaVerificare");
+let indiceSelezionato = selectDaVerificare.selectedIndex;
+let valoreSelezionato = selectDaVerificare.options[indiceSelezionato].value;
+let nomeLivello = selectDaVerificare.options[indiceSelezionato].text;
+console.log(`hai scelto il livello ${nomeLivello} che genera ${valoreSelezionato} celle`);
     containerEl.innerHTML = "";
-    for (let i = 1; i <= cellNumb; i++) {
+    for (let i = 1; i <= valoreSelezionato; i++) {
         const divEl = document.createElement('div')
         divEl.append(i)
         containerEl.append(divEl)
+       
         /*Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
         ed emetto un messaggio in console con il numero della cella cliccata.*/
         divEl.addEventListener('click', function(){
-            console.log('è la cella numero:',i)
+            console.log('quella cliccata è la cella numero:',i)
             this.classList.toggle('bg_light_blue')
         })
+        if (valoreSelezionato == 49) {
+            divEl.style.width = `calc(100% / ${Math.sqrt(valoreSelezionato)} )`
+        }
+        if (valoreSelezionato == 81) {
+            divEl.style.width = `calc(100% / ${Math.sqrt(valoreSelezionato)} )`
+        }
     }
-
-})
-reset.addEventListener('click', function(){
-    containerEl.innerHTML = ''
+    const bombs = genera_bombe(valoreSelezionato)
+    console.log(bombs);
 })
 
-/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà
-prescelta: le bombe.
-nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array
-delle bombe non potranno 
-esserci due numeri uguali.
-In seguito l'utente clicca su una cella:
-se il numero è presente nella lista dei numeri generati
-abbiamo calpestato una bomba
-la cella si colora di rosso e la partita termina.
-Altrimenti
-la cella cliccata si colora di azzurro
-l'utente può continuare a cliccare sulle altre celle. */
+function genera_bombe(max, min = 1) {
+    //Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta  
+    const bombs = [];
+    while (bombs.length < 16) {
+      const bomb = Math.floor(Math.random() * (max - min + 1)) + min;
 
+      if (!bombs.includes(bomb)) {
+        bombs.push(bomb)
+      }
+    }
+    return bombs
+  }
+  
+  
